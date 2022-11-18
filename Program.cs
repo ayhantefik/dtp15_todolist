2d776218d67af8b0e1411ab6ea2b569ac89b6ef5
AYHAN TEFIK <ayhantefik93@gmail.com>
2022-11-18 19:59:29 +0100

    Lade till "beskriv allt"

--- Program.cs
+++ Program.cs
@@ -48,6 +48,10 @@ namespace dtp15_todolist
                 {
                     Console.WriteLine($"{taskDescription,-40}|");
                 }
+                else if (command == "beskriv allt")
+                {
+                    Console.WriteLine($"{taskDescription,-40}|");
+                }
                 else
                     Console.WriteLine();
             }
@@ -107,10 +111,12 @@ namespace dtp15_todolist
             {
                 Console.Write("|status      |prio  |namn                |");
                 if (command == "beskriv") Console.WriteLine("beskrivning                             |");
+                else if (command == "beskriv allt") Console.WriteLine("beskrivning                             |");
                 else Console.WriteLine();
             }
             Console.Write("|------------|------|--------------------|");
             if (command == "beskriv") Console.WriteLine("----------------------------------------|");
+            else if (command == "beskriv allt") Console.WriteLine("----------------------------------------|");
             else Console.WriteLine();
         }
         private static void PrintHead(string command, bool verbose)
@@ -144,61 +150,87 @@ namespace dtp15_todolist
 
             PrintFoot(command, verbose);
         }
-        public static void ChangeToActiv()
+        public static void ChangeToActiv(string command)
         {
-            string aktivera1;
-            Console.Write("Skriv uppgift namn som ska ändras till aktiv: ");
-            aktivera1 = Console.ReadLine();
-            for (int i = 0; i < Todo.list.Count; i++)
+            string status = command.Trim();
+            string[] cwords = status.Split(' ');
+            string expected = $"{cwords[1]} {cwords[2]}";
+            if (status == "")
             {
-                if (aktivera1 == Todo.list[i].task)
+                Console.WriteLine("FEL!");
+            }
+            else
+            {
+                for (int i = 0; i < Todo.list.Count; i++)
                 {
-                    Todo.list[i].status = 1;
+                    if (expected == Todo.list[i].task)
+                    {
+                        Todo.list[i].status = 1;
+                    }
                 }
+                Console.WriteLine($"{cwords[1]} {cwords[2]} har ändrats till aktiv!");
             }
         }
-        public static void ChangeToWaiting()
+        public static void ChangeToWaiting(string command)
         {
-            string waiting2;
-            Console.Write("Skriv uppgift namn som ska ändras till väntande: ");
-            waiting2 = Console.ReadLine();
-            for (int i = 0; i < Todo.list.Count; i++)
+            string status = command.Trim();
+            string[] cwords = status.Split(' ');
+            string expected = $"{cwords[1]} {cwords[2]}";
+            if (status == "")
             {
-                if (waiting2 == Todo.list[i].task)
+                Console.WriteLine("FEL!");
+            }
+            else
+            {
+                for (int i = 0; i < Todo.list.Count; i++)
                 {
-                    Todo.list[i].status = 2;
+                    if (expected == Todo.list[i].task)
+                    {
+                        Todo.list[i].status = 2;
+                    }
                 }
+                Console.WriteLine($"{cwords[1]} {cwords[2]} har ändrats till vänta!");
             }
         }
-        public static void ChangeToReady()
+        public static void ChangeToReady(string command)
         {
-            string ready3;
-            Console.Write("Skriv uppgift namn som ska ändras till avklarad: ");
-            ready3 = Console.ReadLine();
-            for (int i = 0; i < Todo.list.Count; i++)
+            string status = command.Trim();
+            string[] cwords = status.Split(' ');
+            string expected = $"{cwords[1]} {cwords[2]}";
+            if (status == "")
             {
-                if (ready3 == Todo.list[i].task)
-                {
-                    Todo.list[i].status = 3;
-                }
+                Console.WriteLine("FEL!");
             }
+            else
+            {
+                for (int i = 0; i < Todo.list.Count; i++)
+                    {
+                        if (expected == Todo.list[i].task)
+                        {
+                            Todo.list[i].status = 3;
+                        }
+                    }
+                Console.WriteLine($"{cwords[1]} {cwords[2]} har ändrats till klar!");
+            }
+
         }
         public static void PrintHelp()
         {
             Console.WriteLine("Kommandon:");
             Console.WriteLine();
-            Console.WriteLine("hjälp      lista denna hjälp");
+            Console.WriteLine("hjälp        lista denna hjälp");
             Console.WriteLine();
-            Console.WriteLine("ny         skapa en ny uppgift");
-            Console.WriteLine("beskriv    lista alla Active uppgifter och beskrivning");
-            Console.WriteLine("lista      lista alla Active uppgifter");
-            Console.WriteLine("lista allt lista alla uppgifter oavsett status");
-            Console.WriteLine("spara      spara uppgifterna");
-            Console.WriteLine("ladda      ladda listan todo.lis");
-            Console.WriteLine("aktivera   /uppgift/");
-            Console.WriteLine("klar       /uppgift/");
-            Console.WriteLine("vänta      /uppgift/");
-            Console.WriteLine("sluta      spara senast laddade filen och avsluta programmet!");
+            Console.WriteLine("ny           skapa en ny uppgift");
+            Console.WriteLine("beskriv      lista alla Active uppgifter och beskrivning");
+            Console.WriteLine("beskriv allt lista alla uppgifter (oavsätt status) och beskrivning");
+            Console.WriteLine("lista        lista alla Active uppgifter");
+            Console.WriteLine("lista allt   lista alla uppgifter oavsett status");
+            Console.WriteLine("spara        spara uppgifterna");
+            Console.WriteLine("ladda        ladda listan todo.lis");
+            Console.WriteLine("aktivera     /uppgift/");
+            Console.WriteLine("klar         /uppgift/");
+            Console.WriteLine("vänta        /uppgift/");
+            Console.WriteLine("sluta        spara senast laddade filen och avsluta programmet!");
             Console.WriteLine("");
         }
     }
@@ -209,6 +241,7 @@ namespace dtp15_todolist
             Console.WriteLine("Välkommen till att-göra-listan!");
             Todo.PrintHelp();
             string command;
+            string raw;
             do
             {
                 command = MyIO.ReadCommand("> ");
@@ -236,6 +269,9 @@ namespace dtp15_todolist
                 }
                 else if (MyIO.Equals(command, "beskriv"))
                 {
+                    if (MyIO.HasArgument(command, "allt"))
+                        Todo.PrintTodoList(command, verbose: true);
+                    else
                     Todo.PrintTodoList(command, verbose: false);
                 }
                 else if (MyIO.Equals(command, "lista"))
@@ -247,15 +283,15 @@ namespace dtp15_todolist
                 }
                 else if (MyIO.Equals(command, "aktivera"))
                 {
-                    Todo.ChangeToActiv();
+                    Todo.ChangeToActiv(command);
                 }
                 else if (MyIO.Equals(command, "vänta"))
                 {
-                    Todo.ChangeToWaiting();
+                    Todo.ChangeToWaiting(command);
                 }
                 else if (MyIO.Equals(command, "klar"))
                 {
-                    Todo.ChangeToReady();
+                    Todo.ChangeToReady(command);
                 }
                 else
                 {
